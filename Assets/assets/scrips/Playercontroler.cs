9f5f8cab1288a4db0599c41a5f007cb941cca7bb
Travis m <murdocktravis03@gmail.com>
2024-11-18 09:23:08 -0800

    0.2
    added forward force

--- Assets/assets/scrips/Playercontroler.cs
+++ Assets/assets/scrips/Playercontroler.cs
@@ -7,15 +7,23 @@ public class Playercontroler : MonoBehaviour
     [Header("Movement vals")]
     private float horizontalInput;
     [SerializeField] private float turnSpeed;
+    private float verticalInput;
+    [SerializeField] private float thrustfForce;
+    private Rigidbody rb;
     // happens one time in the first frame
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+    private void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        rb.AddRelativeForce(Vector3.forward * verticalInput * thrustfForce);
     }
 }
